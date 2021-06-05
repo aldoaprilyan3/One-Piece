34454696173616060393a857ca787fe433ee593d
aldoaprilyan3 <77969621+aldoaprilyan3@users.noreply.github.com>
2021-06-05 18:54:15 +0700

    Add files via upload

---
+++ userbot/modules/help.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot help command """
+
+import asyncio
+from userbot import CMD_HELP
+from userbot.events import register
+
+modules = CMD_HELP
+
+
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help(event):
+    """ For .help command,"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit("Mohon Maaf Sensei Saya Tidak Memiliki Modul Seperti itu.")
+            await asyncio.sleep(200)
+            await event.delete()
+    else:
+        string = ""
+        for i in CMD_HELP:
+            string += "`" + str(i)
+            string += "`\t➢  "
+        await event.edit("**╔═════☠️ ONE PIECE ☠️══════╗**\n\n"
+                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+                         f"{string}"
+                         "\n╚═══THANKS ALDO GNTENG ⚡️════╝")
+        await event.reply(f"\nCara Menggunakan Contoh: Ketik `.help afk` Untuk Informasi Modules")
+        await asyncio.sleep(1000)
+        await event.delete()
