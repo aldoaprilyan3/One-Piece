193c0420623a8dd967dd323b0e42847edbd9475c
aldoaprilyan3 <77969621+aldoaprilyan3@users.noreply.github.com>
2021-09-05 03:01:31 +0700

    Update trafalgar.py

--- userbot/modules/trafalgar.py
+++ userbot/modules/trafalgar.py
@@ -49,6 +49,43 @@ async def typewriter(typew):
     await typew.edit("[TUTORIAL](https://t.me/tutorbotx)")
 
 
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.1
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "potek":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "❤️",
+
+            "💔",
+
+            "❤️",
+
+            "💔"
+            "‎"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 4])
+
+
 CMD_HELP.update({
     "fitur":
     "`P`\
