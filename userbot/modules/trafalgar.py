db989d1d33de17a18434bb92015c660e765fbb4a
aldoaprilyan3 <77969621+aldoaprilyan3@users.noreply.github.com>
2021-10-26 00:24:50 +0700

    Update trafalgar.py

--- userbot/modules/trafalgar.py
+++ userbot/modules/trafalgar.py
@@ -115,7 +115,7 @@ async def koc(e):
         await e.edit("**Oke Sekian Mksh🙇🏿‍♂🙇🏿‍♂**")
 
 
-@register(outgoing=True, pattern="^.alay$")
+@register(outgoing=True, pattern="^.pp$")
 async def koc(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("**YEU NGENTOT AVA GADA MAKE AKUN FAKE PAKE PP DULU YA TOLOL BIAR BISA LIAT GUA SEBERAPA HINANYA MUKA LU BODOH**")
