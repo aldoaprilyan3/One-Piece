2de05a64ad0e61878728c1744f27bdddf2562555
aldoaprilyan3 <77969621+aldoaprilyan3@users.noreply.github.com>
2021-11-09 17:15:04 +0700

    Update maling.py

--- userbot/modules/maling.py
+++ userbot/modules/maling.py
@@ -24,7 +24,6 @@ from telethon.tl.types import (
     MessageMediaPhoto,
 )
 
-from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot import S_PACK_NAME as custompack
 from userbot import bot
